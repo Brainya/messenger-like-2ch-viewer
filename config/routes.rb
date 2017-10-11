bbc7e3b6ce03729777f14e7766c9c18d9c4b6eed
Brainya <bombird.dtb@gmail.com>
2017-10-11 11:38:55 +0900

    Add subbacks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'subbacks/:subback_id' => 'subbacks#index'
 end
