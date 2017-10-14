46a817bd145f4c2920ad45a762cfbc03f655e770
Brainya <bombird.dtb@gmail.com>
2017-10-14 12:55:54 +0900

    Add index method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,20 @@
 class BoardsController < ApplicationController
   def index
+    agent = Mechanize.new
+    url = 'http://2ch.sc/bbsmenu.html'
+    page = agent.get(url)
+    html = Nokogiri::HTML(page.body)
+    @subbacks = []
+    html.css('small a').each do |subback_title|
+      # subback_url = subback_title.attribute('href').match(/http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?/)
+      # if subback_url.blank?
+      #   next
+      # end
+      p subback_title
+      subback_title['href'] = subback_title['href'].sub('http://', '').insert(0, 'subbacks/')
+      title = subback_title.to_html
+      subback = {title: title}
+      @subbacks << subback
+    end
   end
 end
