63b6ec325390fb93219b57fa7c05e43a18d3d95c
Brainya <bombird.dtb@gmail.com>
2017-10-14 18:44:33 +0900

    Add checkings subback url is subback or not

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,16 +5,17 @@ class BoardsController < ApplicationController
     page = agent.get(url)
     html = Nokogiri::HTML(page.body)
     @subbacks = []
-    html.css('small a').each do |subback_title|
-      # subback_url = subback_title.attribute('href').match(/http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?/)
-      # if subback_url.blank?
-      #   next
-      # end
-      p subback_title
-      subback_title['href'] = subback_title['href'].sub('http://', '').insert(0, 'subbacks/')
-      title = subback_title.to_html
-      subback = {title: title}
-      @subbacks << subback
+    html.css('small a').each do |subback_title|      
+      subback_url = subback_title['href'].match('http(s)?://[a-z0-9.]*.2ch.sc/[a-z0-9]+').to_s
+      case subback_url
+      when '' then
+      when 'http://info.2ch.sc/guide' then # 2ch総合案内
+      else
+        subback_title['href'] = subback_title['href'].sub('http://', '').insert(0, 'subbacks/')
+        title = subback_title.to_html
+        subback = {title: title}
+        @subbacks << subback
+      end
     end
   end
 end
